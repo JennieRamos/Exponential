dd698863456f8e30b79876df869707ecff2b0099
Jennie Ramos <jennieramos06@gmail.com>
2014-09-06 15:41:00 -0700

    final...

---
+++ javascript/globs.js
@@ -0,0 +1,39 @@
+//define functions and global variables here...
+var siteloc = "http://localhost/Exponential";
+var scriptloc = "/scripts/"
+
+
+function fetchExpo()
+{
+  $.ajax({
+      url: siteloc + scriptloc + "make_exponentiater.py",
+
+      data: {
+				 result:$("#inputno").val()
+				
+	    }, 
+      success: function (res) {
+                  $("p").html(" ");
+				  $("p").html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+				  $("p").append(res);
+                  
+              }
+    });
+}
+
+function fetchExpo1()
+{
+  $.ajax({
+      url: siteloc + scriptloc + "make_exponentiater1.py",
+      data: {
+				 result:$("#inputno").val()  
+				
+	    }, 
+      success: function (res) {
+                  $("p").html(" ");
+				  $("p").html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+				  $("p").append(res);
+                  
+}
+    });
+}
