66d1b62cfe3c9c6117d58ef451d1d2e0b341d3db
Jennie Ramos <jennieramos06@gmail.com>
2014-09-05 22:36:44 -0700

    edit

--- javascript/globs.js
+++ javascript/globs.js
@@ -7,13 +7,13 @@ function fetchExpo()
 {
   $.ajax({
       url: siteloc + scriptloc + "make_exponentiater.py",
-<<<<<<< HEAD
+
       data: {
-				 result:$("#inputno.").val()
+				 result:$("#result").val()
 				
 	    } 
       success: function (res) {
-                  console.log(res);
+                  $("p").append(res);
                   
               }
     });
@@ -24,35 +24,12 @@ function fetchExpo1()
   $.ajax({
       url: siteloc + scriptloc + "make_exponentiater1.py",
       data: {
-				 result:$("#inputno.").val()  
+				 result:$("#result").val()  
 				
 	    } 
       success: function (res) {
-                  console.log(res);
+                  $("p").append(res);
                   
-=======
-      data: {e:e,
-             },
-      dataType: 'json',
-      success: function (res) {
-                  console.log(res);
-                  if(res[0][0] != "None")
-                  {
-					  table = '<table border="1">';
-					  for (i = 0; i < res.length; i++)
-					  {
-						  row = res[i];
-						  table += "<tr>";
-						  for (j = 0; j < row.length; j++)
-						  {
-							  table += "<td>" + row[j] + "</td>";
-						  }
-						  table += "</tr>";
-					  }
-					  table += "</table>";
-					  $("#target").html(table); 
-				  } // end if
->>>>>>> 92f1bb2e2b3b97c1825c36e1595f55672d015be8
-              }
+
     });
 }
